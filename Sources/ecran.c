eb7d2a8fdacf41ef324e31e9157b095b2df8711d
LA219263 <la219263@student.helha.be>
2024-03-22 09:49:04 +0100

    Réctification de la fonction AfficherTexteDansCadre() Réctification de la fonction AfficherCharSpecialSasnRetour()

--- Sources/ecran.c
+++ Sources/ecran.c
@@ -95,7 +95,7 @@ void AfficherCharSpecialSansRetour(unsigned char caractereAAfficher, int repetit
     // Afficher caractère spécial x fois sans retour à la ligne
     for (int compteur = 0; compteur < repetition; compteur++)
     {
-        addch(caractereAAfficher);
+        addch(NCURSES_ACS(caractereAAfficher));
     }
     refresh(); // On rafraichit l'écran pour être sûr du changement
 }
@@ -105,30 +105,29 @@ void AfficherCharSpecialSansRetour(unsigned char caractereAAfficher, int repetit
 // - la chaine de caracteres a afficher dans le cadre (pointeur)
 void AfficherTexteDansCadre(char *texteAAfficher)
 {
-    int i,len=strlen(texteAAfficher)+2;
-    //affichage de la ligne suppérieur du cadre
-    printw(" ");
-    addch(NCURSES_ACS('l'));
-    for(i=0;i<(len);i++){
-        addch(NCURSES_ACS('q'));
-    }
-    addch(NCURSES_ACS('k'));
-    
-    //affichage de la ligne centrale du cadre avec le texte
-    printw("\n ");
-    addch(NCURSES_ACS('x'));
-    printw(" %s ",texteAAfficher);
-    addch(NCURSES_ACS('x'));
+    int longeurTexte = strlen(texteAAfficher)+2; // On prends la longeur du texte + 2 pour les coins
 
-    //affichage de la ligne inférieur du cadre
-    printw("\n ");
-    addch(NCURSES_ACS('m'));
-    for(i=0;i<len;i++){
-        addch(NCURSES_ACS('q'));
-    }
-    addch(NCURSES_ACS('j'));
-    printw("\n\n");
-    refresh();
+    // Affichage de la ligne suppérieur du cadre
+    AfficherCharSpecialSansRetour('l', 1); // On affiche le coin supérieur gauche
+    AfficherCharSpecialSansRetour('q', longeurTexte); // On affihce la ligne du haut
+    AfficherCharSpecialSansRetour('k', 1); // On affiche le coin supérieur droit
+    
+    // Affichage de la ligne centrale du cadre avec le texte
+    RetourALaLigne();
+    AfficherCharSpecialSansRetour('x', 1); // On affiche la barre gauche
+    AfficherCharSansRetour(' ', 1); // On affiche 3 espace pour la séparation
+    AfficherTexteSansRetour(texteAAfficher); // On affiche le texte
+    AfficherCharSansRetour(' ', 1); // On affiche 1 espace pour la séparation
+    AfficherCharSpecialSansRetour('x', 1);  // On affiche la barre droite
+
+    // Affichage de la ligne inférieur du cadre
+    RetourALaLigne(); // On fais un retour à la ligne
+    AfficherCharSpecialSansRetour('m', 1);  // On affiche le coin inférieur gauche
+    AfficherCharSpecialSansRetour('q', longeurTexte); // On affiche la ligne du bas
+    AfficherCharSpecialSansRetour('j', 1); // On affiche le coin inférieur droit
+    RetourALaLigne(); // On fais un retour à la ligne
+    RetourALaLigne(); // On fais un retour à la ligne
+    refresh(); // On rafraichît l'écran pour être sûr du changement
 }
 
 // Cette fonction efface l'ecran, affiche "Erreur" dans un cadre
