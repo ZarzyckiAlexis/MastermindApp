a0e487b7a29602671d5f4ba99ae340a4bcb1e62b
LA219263 <la219263@student.helha.be>
2024-03-22 08:46:34 +0100

    Implémentation de la fonction LireText() Fin de clavier.c

--- Sources/clavier.c
+++ Sources/clavier.c
@@ -5,11 +5,10 @@
 // Renvoie:
 // - Le pointeur vers de la mémoire allouée avec la chaîne de caractères
 // Note: cette chaîne devra être libérée par l'appelant plus tard
-char *LireTexte()
-{
-   // FONCTION UTILISEE:
-   // Cf LPP2 - ncurses
-   // scanw()
-
-   return NULL; // A adapter
+char *LireTexte(){
+   char buffer[256]; // Pour stocker temporairement la chaîne de caractère
+   scanw("%255s", buffer); // On précise %255s pour éviter le overflow
+   char *texte = malloc(sizeof(strlen(buffer) + 1)); // On prends la taille réel du buffer pour le malloc + 1 pour le null byte
+   strcpy(texte, buffer); // On copie le contenu du buffer dans notre texte
+   return texte; // On renvoie le pointeur
 }
